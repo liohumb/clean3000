7771a8107c11aac454c4c2fc8655edcc924722b9
liohumb <helloliohumb@gmail.com>
2023-01-09 16:16:17 +0100

    mise en place de la vue des avis & ajout de message et class active dans le formulaire

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,6 +2,8 @@ import './table.scss'
 import { Link } from 'react-router-dom'
 
 export default function Table() {
+    const avis = JSON.parse(localStorage.getItem('avis')).reverse()
+
     return (
         <ul className="table">
             <li className="table__header">
@@ -10,19 +12,18 @@ export default function Table() {
                 <div className="table__col table__col-content">Observations</div>
                 <div className="table__col table__col-options"><i className="bx bxs-cog"/></div>
             </li>
-            <li className="table__row">
-                <Link to="/avis-de-passage/:id" className="table__col table__col-name" data-label="Nom">John Doe</Link>
-                <Link to="/avis-de-passage/:id" className="table__col table__col-date" data-label="Date">{Date.now()}</Link>
-                <Link to="/avis-de-passage/:id" className="table__col table__col-content" data-label="Observation">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Delectus eius et exercitationem, ipsam laboriosam minus possimus quos reprehenderit similique sunt
-                    veritatis vero. Eaque enim facilis magnam nam nulla, officiis sapiente.
-                </Link>
-                <div className="table__col table__col-options">
-                    <i className="bx bxs-edit table__col-edit"/>
-                    <span> | </span>
-                    <i className='bx bxs-trash table__col-delete'></i>
-                </div>
-            </li>
+            {avis.map( ( item ) => (
+                <li className="table__row" key={item.id}>
+                    <Link to="/avis-de-passage/:id" className="table__col table__col-name" data-label="Nom">{item.name}</Link>
+                    <Link to="/avis-de-passage/:id" className="table__col table__col-date" data-label="Date">{item.date}</Link>
+                    <Link to="/avis-de-passage/:id" className="table__col table__col-content" data-label="Observation">{item.content}</Link>
+                    <div className="table__col table__col-options">
+                        <i className="bx bxs-edit table__col-edit"/>
+                        <span> | </span>
+                        <i className='bx bxs-trash table__col-delete'></i>
+                    </div>
+                </li>
+            ))}
         </ul>
     )
 }
--- src/navigations/footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './footer.scss'
-
-export default function Footer(  ) {
-    return (
-        <footer className="footer">
-            
-        </footer>
-    )
-}
